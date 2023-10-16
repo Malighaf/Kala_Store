c6b2553a3b97b013ae6bd8c9005e471b24e346b8
malighaf <info.saghalain@gmail.com>
2023-10-16 13:30:47 +0330

    Kala

---
+++ Bugeto_Store.Application/Services/Products/Commands/DeleteCategory/DeleteCategoryService.cs
@@ -0,0 +1,46 @@
+﻿using Bugeto_Store.Application.Interfaces.Contexts;
+using Bugeto_Store.Application.Services.Products.Commands.AddNewCategory;
+using Bugeto_Store.Application.Services.Users.Commands.RemoveUser;
+using Bugeto_Store.Common.Dto;
+using Bugeto_Store.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugeto_Store.Application.Services.Products.Commands.DeleteCategory
+{
+    public  class DeleteCategoryService : IDeleteCategoryService
+    {
+        private readonly IDataBaseContext _context;
+
+        public DeleteCategoryService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+
+        public ResultDto Execute(long id )
+        {
+
+            var user = _context.Categories.Find(id);
+            if (user == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد"
+                };
+            }
+            user.RemoveTime = DateTime.Now;
+            user.IsRemoved = true;
+            _context.SaveChanges();
+            return new ResultDto()
+            {
+                IsSuccess = true,
+                Message = "کاربر با موفقیت حذف شد"
+            };
+        }
+    }
+}
---
+++ Bugeto_Store.Persistence/Migrations/20230908124737_aadsf.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bugeto_Store.Persistence.Migrations
+{
+    public partial class aadsf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "InsertTime",
+                value: new DateTime(2023, 9, 8, 16, 17, 37, 374, DateTimeKind.Local).AddTicks(5418));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "InsertTime",
+                value: new DateTime(2023, 9, 8, 16, 17, 37, 376, DateTimeKind.Local).AddTicks(3834));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "InsertTime",
+                value: new DateTime(2023, 9, 8, 16, 17, 37, 376, DateTimeKind.Local).AddTicks(3959));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "InsertTime",
+                value: new DateTime(2020, 8, 16, 12, 52, 49, 673, DateTimeKind.Local).AddTicks(9573));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "InsertTime",
+                value: new DateTime(2020, 8, 16, 12, 52, 49, 679, DateTimeKind.Local).AddTicks(809));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "InsertTime",
+                value: new DateTime(2020, 8, 16, 12, 52, 49, 679, DateTimeKind.Local).AddTicks(1089));
+        }
+    }
+}
